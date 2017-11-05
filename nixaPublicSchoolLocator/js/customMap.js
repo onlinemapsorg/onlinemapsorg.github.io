d03ffe62b6ec385d2f82e4a01caf5f843e8429f4
schuylerwilliams <sky@Sky-MacBook-Air.local>
2017-11-05 17:05:55 -0600

    test

--- nixaPublicSchoolLocator/js/customMap.js
+++ nixaPublicSchoolLocator/js/customMap.js
@@ -56,6 +56,7 @@ var nixaPublicSchoolLocator = (function () {
 				intermediate_boundary_layer.addGeoJson(data.features[i]);
 				var testList = [];
 				var f1 = features[i].geometry.coordinates;
+				console.log(f1);
 				var i1;
 				for (i1 = 0; i1 < features[i].geometry.coordinates.length; i1++) {
 					var f2 = f1[i1];
@@ -80,8 +81,6 @@ var nixaPublicSchoolLocator = (function () {
 				elementary_boundaries_layer.addGeoJson(data.features[i]);
 				var testList = [];
 				var f1 = features[i].geometry.coordinates[0];
-				var f2 = features[i].geometry.coordinates[1];
-				console.log(f2);
 				var i1;
 				for (i1 = 0; i1 < features[i].geometry.coordinates[0].length; i1++) {
 					var f2 = f1[i1];
