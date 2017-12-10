9aa8ccf68800f1c66ede14f9418e4b7e98eac45c
skywilliamsd9 <schuylerwilliams@Skys-iMac.local>
2017-12-10 17:16:56 -0600

    update

--- northKansasCityPublicSchools/js/customMap.js
+++ northKansasCityPublicSchools/js/customMap.js
@@ -24,21 +24,8 @@ var northKansasCityPublicSchools = (function () {
 			url: "../data/NKC_Points_proj_t.csv",
 			datatype: "text",
 			success: function(data) {
-				var allTextLines = allText.split(/\r\n|\n/);
-    			var headers = allTextLines[0].split(',');
-    			var lines = [];
-
-    				for (var i=1; i<allTextLines.length; i++) {
-        				var data = allTextLines[i].split(',');
-        				if (data.length == headers.length) {
-            				var tarr = [];
-            				for (var j=0; j<headers.length; j++) {
-                				tarr.push(headers[j]+":"+data[j]);
-            				}
-            				lines.push(tarr);
-        				}
-   					}
-   				console.log(lines);
+				var dat = $.csv.toObjects(data);
+				console.log(dat);
 			}
 		});
 		// construct map
